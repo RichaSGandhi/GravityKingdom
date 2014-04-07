8bcd889ca9a1d081147892b55f5a6fbf315cd038
DIVMS <divms@divms-m012.pk5001z>
2014-04-06 20:27:31 -0500

    Initial Commit

---
+++ GravityKingdom/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GravityKingdom
+//
+//  Created by DIVMS on 4/6/14.
+//  Copyright (c) 2014 uiowa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
